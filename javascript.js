7cbeb0074078eb8d2c96c1f5e0a896347a4e52c0
ruiz19746 <ruiz19746@iesmarenostrum.com>
2019-11-19 12:25:03 +0100

    segundo stikcy

--- javascript.js
+++ javascript.js
@@ -1,7 +1,24 @@
-var $window = $(window),
-    $nav = $('#navigation'),
-    navTop = $nav.offset().top;
+// var $window = $(window),
+//     $nav = $('#navigation'),
+//     navTop = $nav.offset().top;
 
-$window.scroll(function() {
-    $nav.toggleClass('sticky', $window.scrollTop() > navTop);
+// $window.scroll(function() {
+//     $nav.toggleClass('sticky', $window.scrollTop() > navTop);
+// });
+
+
+/**/
+
+$(document).ready(function() {
+    var stickyNavTop = $('nav').offset().top;
+
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > stickyNavTop) {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    });
 });
